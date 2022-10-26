92b0158f965cf16c5ff3fe69734f6add37230c3f
zoufrank <zoufrank@localhost>
2022-10-26 17:36:11 +0800

    2022.10.26

--- pwd-tools/src/main/java/com/example/pwdtools/PwdToolsApplication.java
+++ pwd-tools/src/main/java/com/example/pwdtools/PwdToolsApplication.java
@@ -4,6 +4,7 @@ import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PwdToolsApplication {
 	public BasicTextEncryptor textEncryptor;
